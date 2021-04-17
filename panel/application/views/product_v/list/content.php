f6ebc9277265adc0411e162455ae45ce960e52a2
nurcan1 <nrcnplt93@gmail.com>
2021-04-18 02:58:24 +0300

    isActiveSetter islemi hem controller'da hem de custom.js uzerinde gerceklestirildi.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -30,7 +30,7 @@
                                 <td><?php echo $item->title; ?></td>
                                 <td><?php echo $item->description; ?></td>
                                 <td>
-                                    <input type="checkbox" data-switchery data-color="#10c469" <?php echo ($item->isActive) ? "checked" :  " "; ?> />
+                                    <input data-url="<?php echo base_url("product/isActiveSetter/$item->id"); ?>" class="isActive" type="checkbox" data-switchery data-color="#10c469" <?php echo ($item->isActive) ? "checked" :  " "; ?> />
                                 </td>
                                 <td>
                                     <a data-url="<?php echo base_url("product/delete/$item->id") ?>" type="button" class="btn btn-sm btn-danger btn-outline remove-btn"><i class="fa fa-trash"></i> Sil</a>
--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $(".remove-btn").click(function(e) {
+    $(".remove-btn").click(function() {
 
         var $data_url = $(this).data("url");
 
@@ -23,4 +23,15 @@ $(document).ready(function() {
 
     })
 
+    $(".isActive").change(function() {
+
+        var $data = $(this).prop("checked");
+        var $data_url = $(this).data("url");
+
+        if (typeof $data !== "undefined" && typeof $data_url !== "undefined") {
+            $.post($data_url, { data: $data }, function(response) {
+
+            });
+        }
+    });
 })
