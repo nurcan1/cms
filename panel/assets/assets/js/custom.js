c4d96102873e2847b688e7d361bfca42d39f5a34
nurcan1 <nrcnplt93@gmail.com>
2021-04-18 02:13:33 +0300

    sweetalert  entegre edildi ve custom.js dosyası eklendi.

---
+++ panel/assets/assets/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    swal({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        type: 'error',
+        confirmButtonText: 'Cool'
+    })
+
+})
