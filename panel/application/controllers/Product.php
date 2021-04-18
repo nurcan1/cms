8ed0307d3b3c8b03941f31d2970a171691d55b2c
nurcan1 <nrcnplt93@gmail.com>
2021-04-18 17:39:17 +0300

    upload edilen dosyanın isminin düzenlenerek veri tabanına kaydının sağlanması

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -2,276 +2,273 @@
 
 class Product extends CI_Controller
 {
-    public $viewFolder = "";
+	public $viewFolder = "";
 
-    public function __construct()
-    {
+	public function __construct()
+	{
 
-        parent::__construct();
+		parent::__construct();
 
-        $this->viewFolder = "product_v";
+		$this->viewFolder = "product_v";
 
-        $this->load->model("product_model");
-        $this->load->model("product_image_model");
+		$this->load->model("product_model");
+		$this->load->model("product_image_model");
+	}
 
-    }
+	public function index()
+	{
 
-    public function index(){
+		$viewData = new stdClass();
 
-        $viewData = new stdClass();
+		/** Tablodan Verilerin Getirilmesi.. */
+		$items = $this->product_model->get_all(
+			array(),
+			"rank ASC"
+		);
 
-        /** Tablodan Verilerin Getirilmesi.. */
-        $items = $this->product_model->get_all(
-            array(), "rank ASC"
-        );
+		/** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+		$viewData->viewFolder = $this->viewFolder;
+		$viewData->subViewFolder = "list";
+		$viewData->items = $items;
 
-        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-        $viewData->viewFolder = $this->viewFolder;
-        $viewData->subViewFolder = "list";
-        $viewData->items = $items;
+		$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+	}
 
-        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-    }
+	public function new_form()
+	{
 
-    public function new_form(){
+		$viewData = new stdClass();
 
-        $viewData = new stdClass();
+		/** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+		$viewData->viewFolder = $this->viewFolder;
+		$viewData->subViewFolder = "add";
 
-        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-        $viewData->viewFolder = $this->viewFolder;
-        $viewData->subViewFolder = "add";
+		$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+	}
 
-        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+	public function save()
+	{
 
-    }
+		$this->load->library("form_validation");
 
-    public function save(){
+		// Kurallar yazilir..
+		$this->form_validation->set_rules("title", "Başlık", "required|trim");
 
-        $this->load->library("form_validation");
+		$this->form_validation->set_message(
+			array(
+				"required"  => "<b>{field}</b> alanı doldurulmalıdır"
+			)
+		);
 
-        // Kurallar yazilir..
-        $this->form_validation->set_rules("title", "Başlık", "required|trim");
+		// Form Validation Calistirilir..
+		// TRUE - FALSE
+		$validate = $this->form_validation->run();
 
-        $this->form_validation->set_message(
-            array(
-                "required"  => "<b>{field}</b> alanı doldurulmalıdır"
-            )
-        );
+		// Monitör Askısı
+		// monitor-askisi
 
-        // Form Validation Calistirilir..
-        // TRUE - FALSE
-        $validate = $this->form_validation->run();
+		if ($validate) {
 
-        // Monitör Askısı
-        // monitor-askisi
+			$insert = $this->product_model->add(
+				array(
+					"title"         => $this->input->post("title"),
+					"description"   => $this->input->post("description"),
+					"url"           => convertToSEO($this->input->post("title")),
+					"rank"          => 0,
+					"isActive"      => 1,
+					"createdAt"     => date("Y-m-d H:i:s")
+				)
+			);
 
-        if($validate){
+			// TODO Alert sistemi eklenecek...
+			if ($insert) {
 
-            $insert = $this->product_model->add(
-                array(
-                    "title"         => $this->input->post("title"),
-                    "description"   => $this->input->post("description"),
-                    "url"           => convertToSEO($this->input->post("title")),
-                    "rank"          => 0,
-                    "isActive"      => 1,
-                    "createdAt"     => date("Y-m-d H:i:s")
-                )
-            );
+				redirect(base_url("product"));
+			} else {
 
-            // TODO Alert sistemi eklenecek...
-            if($insert){
+				redirect(base_url("product"));
+			}
+		} else {
 
-                redirect(base_url("product"));
+			$viewData = new stdClass();
 
-            } else {
+			/** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+			$viewData->viewFolder = $this->viewFolder;
+			$viewData->subViewFolder = "add";
+			$viewData->form_error = true;
 
-                redirect(base_url("product"));
+			$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+		}
 
-            }
+		// Başarılı ise
+		// Kayit işlemi baslar
+		// Başarısız ise
+		// Hata ekranda gösterilir...
+
+	}
 
-        } else {
+	public function update_form($id)
+	{
+
+		$viewData = new stdClass();
+
+		/** Tablodan Verilerin Getirilmesi.. */
+		$item = $this->product_model->get(
+			array(
+				"id"    => $id,
+			)
+		);
 
-            $viewData = new stdClass();
+		/** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+		$viewData->viewFolder = $this->viewFolder;
+		$viewData->subViewFolder = "update";
+		$viewData->item = $item;
+
+		$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+	}
+
+	public function update($id)
+	{
+
+		$this->load->library("form_validation");
+
+		// Kurallar yazilir..
+		$this->form_validation->set_rules("title", "Başlık", "required|trim");
+
+		$this->form_validation->set_message(
+			array(
+				"required"  => "<b>{field}</b> alanı doldurulmalıdır"
+			)
+		);
 
-            /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-            $viewData->viewFolder = $this->viewFolder;
-            $viewData->subViewFolder = "add";
-            $viewData->form_error = true;
+		// Form Validation Calistirilir..
+		// TRUE - FALSE
+		$validate = $this->form_validation->run();
 
-            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-        }
+		// Monitör Askısı
+		// monitor-askisi
 
-        // Başarılı ise
-            // Kayit işlemi baslar
-        // Başarısız ise
-            // Hata ekranda gösterilir...
+		if ($validate) {
 
-    }
+			$update = $this->product_model->update(
+				array(
+					"id"    => $id
+				),
+				array(
+					"title"         => $this->input->post("title"),
+					"description"   => $this->input->post("description"),
+					"url"           => convertToSEO($this->input->post("title")),
+				)
+			);
 
-    public function update_form($id){
+			// TODO Alert sistemi eklenecek...
+			if ($update) {
+
+				redirect(base_url("product"));
+			} else {
 
-        $viewData = new stdClass();
-
-        /** Tablodan Verilerin Getirilmesi.. */
-        $item = $this->product_model->get(
-            array(
-                "id"    => $id,
-            )
-        );
-        
-        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-        $viewData->viewFolder = $this->viewFolder;
-        $viewData->subViewFolder = "update";
-        $viewData->item = $item;
-
-        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-
-
-    }
-
-    public function update($id){
-
-        $this->load->library("form_validation");
-
-        // Kurallar yazilir..
-        $this->form_validation->set_rules("title", "Başlık", "required|trim");
-
-        $this->form_validation->set_message(
-            array(
-                "required"  => "<b>{field}</b> alanı doldurulmalıdır"
-            )
-        );
-
-        // Form Validation Calistirilir..
-        // TRUE - FALSE
-        $validate = $this->form_validation->run();
-
-        // Monitör Askısı
-        // monitor-askisi
-
-        if($validate){
-
-            $update = $this->product_model->update(
-                array(
-                    "id"    => $id
-                ),
-                array(
-                    "title"         => $this->input->post("title"),
-                    "description"   => $this->input->post("description"),
-                    "url"           => convertToSEO($this->input->post("title")),
-                )
-            );
-
-            // TODO Alert sistemi eklenecek...
-            if($update){
-
-                redirect(base_url("product"));
-
-            } else {
-
-                redirect(base_url("product"));
-
-            }
-
-        } else {
-
-            $viewData = new stdClass();
-
-            /** Tablodan Verilerin Getirilmesi.. */
-            $item = $this->product_model->get(
-                array(
-                    "id"    => $id,
-                )
-            );
-
-            /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-            $viewData->viewFolder = $this->viewFolder;
-            $viewData->subViewFolder = "update";
-            $viewData->form_error = true;
-            $viewData->item = $item;
-
-            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-        }
-
-        // Başarılı ise
-        // Kayit işlemi baslar
-        // Başarısız ise
-        // Hata ekranda gösterilir...
-
-    }
-
-    public function delete($id){
-
-        $delete = $this->product_model->delete(
-            array(
-                "id"    => $id
-            )
-        );
-
-        // TODO Alert Sistemi Eklenecek...
-        if($delete){
-            redirect(base_url("product"));
-        } else {
-            redirect(base_url("product"));
-        }
-
-    }
-
-    public function isActiveSetter($id){
-
-        if($id){
-
-            $isActive = ($this->input->post("data") === "true") ? 1 : 0;
-
-            $this->product_model->update(
-                array(
-                    "id"    => $id
-                ),
-                array(
-                    "isActive"  => $isActive
-                )
-            );
-        }
-    }
-
-    public function rankSetter(){
-
-
-        $data = $this->input->post("data");
-
-        parse_str($data, $order);
-
-        $items = $order["ord"];
-
-        foreach ($items as $rank => $id){
-
-            $this->product_model->update(
-                array(
-                    "id"        => $id,
-                    "rank !="   => $rank
-                ),
-                array(
-                    "rank"      => $rank
-                )
-            );
-
-        }
-
-    }
-
-    public function image_form($id){
-
-        $viewData = new stdClass();
-
-        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-        $viewData->viewFolder = $this->viewFolder;
-        $viewData->subViewFolder = "image";
-
-        $viewData->item = $this->product_model->get(
-            array(
-                "id"    => $id
-            )
-        );
+				redirect(base_url("product"));
+			}
+		} else {
+
+			$viewData = new stdClass();
+
+			/** Tablodan Verilerin Getirilmesi.. */
+			$item = $this->product_model->get(
+				array(
+					"id"    => $id,
+				)
+			);
+
+			/** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+			$viewData->viewFolder = $this->viewFolder;
+			$viewData->subViewFolder = "update";
+			$viewData->form_error = true;
+			$viewData->item = $item;
+
+			$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+		}
+
+		// Başarılı ise
+		// Kayit işlemi baslar
+		// Başarısız ise
+		// Hata ekranda gösterilir...
+
+	}
+
+	public function delete($id)
+	{
+
+		$delete = $this->product_model->delete(
+			array(
+				"id"    => $id
+			)
+		);
+
+		// TODO Alert Sistemi Eklenecek...
+		if ($delete) {
+			redirect(base_url("product"));
+		} else {
+			redirect(base_url("product"));
+		}
+	}
+
+	public function isActiveSetter($id)
+	{
+
+		if ($id) {
+
+			$isActive = ($this->input->post("data") === "true") ? 1 : 0;
+
+			$this->product_model->update(
+				array(
+					"id"    => $id
+				),
+				array(
+					"isActive"  => $isActive
+				)
+			);
+		}
+	}
+
+	public function rankSetter()
+	{
+
+
+		$data = $this->input->post("data");
+
+		parse_str($data, $order);
+
+		$items = $order["ord"];
+
+		foreach ($items as $rank => $id) {
+
+			$this->product_model->update(
+				array(
+					"id"        => $id,
+					"rank !="   => $rank
+				),
+				array(
+					"rank"      => $rank
+				)
+			);
+		}
+	}
+
+	public function image_form($id)
+	{
+
+		$viewData = new stdClass();
+
+		/** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+		$viewData->viewFolder = $this->viewFolder;
+		$viewData->subViewFolder = "image";
+
+		$viewData->item = $this->product_model->get(
+			array(
+				"id"    => $id
+			)
+		);
 
 		$viewData->item_images = $this->product_image_model->get_all(
 			array(
@@ -279,39 +276,38 @@ class Product extends CI_Controller
 			)
 		);
 
-        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+		$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+	}
 
-    }
+	public function image_upload($id)
+	{
+		//upload ismini düzenlemek ve karışıklıkları engellemek için oluşturulan kod
+		$file_name = pathinfo($_FILES["file"]["name"], PATHINFO_FILENAME) . "." . pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
 
-    public function image_upload($id){
+		$config["allowed_types"] = "jpg|jpeg|png";
+		$config["upload_path"]   = "uploads/$this->viewFolder/";
+		$config["file_name"]     = $file_name;
 
-        $config["allowed_types"] = "jpg|jpeg|png";
-        $config["upload_path"]   = "uploads/$this->viewFolder/";
+		$this->load->library("upload", $config);
 
-        $this->load->library("upload", $config);
+		$upload = $this->upload->do_upload("file");
 
-        $upload = $this->upload->do_upload("file");
+		if ($upload) {
 
-        if($upload){
+			$uploaded_file = $this->upload->data("file_name");
 
-            $uploaded_file = $this->upload->data("file_name");
-
-            $this->product_image_model->add(
-                array(
-                    "img_url"       => $uploaded_file,
-                    "rank"          => 0,
-                    "isActive"      => 1,
-                    "isCover"       => 0,
-                    "createdAt"     => date("Y-m-d H:i:s"),
-                    "product_id"    => $id
-                )
-            );
-
-
-        } else {
-            echo "islem basarisiz";
-        }
-
-    }
-
+			$this->product_image_model->add(
+				array(
+					"img_url"       => $uploaded_file,
+					"rank"          => 0,
+					"isActive"      => 1,
+					"isCover"       => 0,
+					"createdAt"     => date("Y-m-d H:i:s"),
+					"product_id"    => $id
+				)
+			);
+		} else {
+			echo "islem basarisiz";
+		}
+	}
 }
