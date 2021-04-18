9f80d81dfad1f29109005dc10a7409d003952a4d
nurcan1 <nrcnplt93@gmail.com>
2021-04-18 15:15:00 +0300

    css ile urun fotograflarinin tasariminda degisiklikler yapildi.

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -1,10 +1,5 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Ürünün Fotoğrafları
-            <a href="#" class="btn btn-outline btn-primary btn-xs pull-right"> <i class="fa fa-plus"></i> Yeni Ekle</a>
-        </h4>
-    </div><!-- END column -->
-    <div class="col-md-12">
         <div class="widget">
 
             <div class="widget-body">
@@ -30,7 +25,7 @@
         <div class="widget">
 
             <div class="widget-body">
-                <table class="table table-bordered table-striped table-hover">
+                <table class="table table-bordered table-striped table-hover pictures_list">
                     <thead>
                         <th>#id</th>
                         <th>Görsel</th>
@@ -40,17 +35,17 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td>#1</td>
-                            <td>
+                            <td class="w100 text-center">#1</td>
+                            <td class="w100">
                                 <img class="img-responsive" width="50" src="https://i.pinimg.com/originals/83/1a/72/831a7203d26d1cd11604c2bcbbdaabea.png" alt="">
                             </td>
                             <td>deneme.jpg</td>
-                            <td>
+                            <td class="w100 text-center">
                                 <input data-url="<?php echo base_url("product/isActiveSetter/"); ?>" class="isActive" type="checkbox" data-switchery data-color="#10c469" <?php echo (true) ? "checked" :  " "; ?> />
 
                             </td>
-                            <td>
-                                <a data-url="<?php echo base_url("product/delete/") ?>" type="button" class="btn btn-sm btn-danger btn-outline remove-btn"><i class="fa fa-trash"></i> Sil</a>
+                            <td class="w100 text-center">
+                                <a data-url="<?php echo base_url("product/delete/") ?>" type="button" class="btn btn-sm btn-danger btn-outline btn-block remove-btn"><i class="fa fa-trash"></i> Sil</a>
 
                             </td>
                         </tr>
