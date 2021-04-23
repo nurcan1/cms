163d6107139848df2f4e972b9d4ec1dc269607f7
nurcan1 <nrcnplt93@gmail.com>
2021-04-24 02:48:33 +0300

    Referans update aksiyonlarının yapilmasi

--- panel/application/controllers/References.php
+++ panel/application/controllers/References.php
@@ -178,85 +178,6 @@ class References extends CI_Controller
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
     }
 
-    public function update_($id)
-    {
-
-        $this->load->library("form_validation");
-
-        // Kurallar yazilir..
-        $this->form_validation->set_rules("title", "Başlık", "required|trim");
-
-        $this->form_validation->set_message(
-            array(
-                "required"  => "<b>{field}</b> alanı doldurulmalıdır"
-            )
-        );
-
-        // Form Validation Calistirilir..
-        // TRUE - FALSE
-        $validate = $this->form_validation->run();
-
-        // Monitör Askısı
-        // monitor-askisi
-
-        if ($validate) {
-
-            $update = $this->reference_model->update(
-                array(
-                    "id"    => $id
-                ),
-                array(
-                    "title"         => $this->input->post("title"),
-                    "description"   => $this->input->post("description"),
-                    "url"           => convertToSEO($this->input->post("title")),
-                )
-            );
-
-            // TODO Alert sistemi eklenecek...
-            if ($update) {
-
-                $alert = array(
-                    "title" => "İşlem Başarılı",
-                    "text" => "Kayıt başarılı bir şekilde güncellendi",
-                    "type"  => "success"
-                );
-            } else {
-
-                $alert = array(
-                    "title" => "İşlem Başarılı",
-                    "text" => "Güncelleme sırasında bir problem oluştu",
-                    "type"  => "error"
-                );
-            }
-
-            $this->session->set_flashdata("alert", $alert);
-            redirect(base_url("product"));
-        } else {
-
-            $viewData = new stdClass();
-
-            /** Tablodan Verilerin Getirilmesi.. */
-            $item = $this->reference_model->get(
-                array(
-                    "id"    => $id,
-                )
-            );
-
-            /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-            $viewData->viewFolder = $this->viewFolder;
-            $viewData->subViewFolder = "update";
-            $viewData->form_error = true;
-            $viewData->item = $item;
-
-            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-        }
-
-        // Başarılı ise
-        // Kayit işlemi baslar
-        // Başarısız ise
-        // Hata ekranda gösterilir...
-
-    }
 
     public function update($id)
     {
@@ -265,13 +186,6 @@ class References extends CI_Controller
 
         // Kurallar yazilir..
 
-        $references_type = $this->input->post("references_type");
-
-        if ($references_type == "video") {
-
-            $this->form_validation->set_rules("video_url", "Video URL", "required|trim");
-        }
-
         $this->form_validation->set_rules("title", "Başlık", "required|trim");
 
         $this->form_validation->set_message(
@@ -285,66 +199,49 @@ class References extends CI_Controller
 
         if ($validate) {
 
-            if ($references_type == "image") {
+            // Upload Süreci...
+            if ($_FILES["img_url"]["name"] !== "") {
 
-                // Upload Süreci...
+                $file_name = convertToSEO(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
 
+                $config["allowed_types"] = "jpg|jpeg|png";
+                $config["upload_path"] = "uploads/$this->viewFolder/";
+                $config["file_name"] = $file_name;
 
-                if ($_FILES["img_url"]["name"] !== "") {
+                $this->load->library("upload", $config);
 
-                    $file_name = convertToSEO(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
+                $upload = $this->upload->do_upload("img_url");
 
-                    $config["allowed_types"] = "jpg|jpeg|png";
-                    $config["upload_path"] = "uploads/$this->viewFolder/";
-                    $config["file_name"] = $file_name;
+                if ($upload) {
 
-                    $this->load->library("upload", $config);
-
-                    $upload = $this->upload->do_upload("img_url");
-
-                    if ($upload) {
-
-                        $uploaded_file = $this->upload->data("file_name");
-
-                        $data = array(
-                            "title" => $this->input->post("title"),
-                            "description" => $this->input->post("description"),
-                            "url" => convertToSEO($this->input->post("title")),
-                            "references_type" => $references_type,
-                            "img_url" => $uploaded_file,
-                            "video_url" => "#",
-                        );
-                    } else {
-
-                        $alert = array(
-                            "title" => "İşlem Başarısız",
-                            "text" => "Görsel yüklenirken bir problem oluştu",
-                            "type" => "error"
-                        );
-
-                        $this->session->set_flashdata("alert", $alert);
-
-                        redirect(base_url("references/update_form/$id"));
-
-                        die();
-                    }
-                } else {
+                    $uploaded_file = $this->upload->data("file_name");
 
                     $data = array(
                         "title" => $this->input->post("title"),
                         "description" => $this->input->post("description"),
                         "url" => convertToSEO($this->input->post("title")),
+                        "img_url" => $uploaded_file,
                     );
+                } else {
+
+                    $alert = array(
+                        "title" => "İşlem Başarısız",
+                        "text" => "Görsel yüklenirken bir problem oluştu",
+                        "type" => "error"
+                    );
+
+                    $this->session->set_flashdata("alert", $alert);
+
+                    redirect(base_url("references/update_form/$id"));
+
+                    die();
                 }
-            } else if ($references_type == "video") {
+            } else {
 
                 $data = array(
-                    "title"         => $this->input->post("title"),
-                    "description"   => $this->input->post("description"),
-                    "url"           => convertToSEO($this->input->post("title")),
-                    "references_type"     => $references_type,
-                    "img_url"       => "#",
-                    "video_url"     => $this->input->post("video_url")
+                    "title" => $this->input->post("title"),
+                    "description" => $this->input->post("description"),
+                    "url" => convertToSEO($this->input->post("title")),
                 );
             }
 
@@ -379,7 +276,6 @@ class References extends CI_Controller
             $viewData->viewFolder = $this->viewFolder;
             $viewData->subViewFolder = "update";
             $viewData->form_error = true;
-            $viewData->references_type = $references_type;
 
             /** Tablodan Verilerin Getirilmesi.. */
             $viewData->item = $this->reference_model->get(
--- panel/application/views/references_v/update/content.php
+++ panel/application/views/references_v/update/content.php
@@ -9,7 +9,7 @@
 
             <div class="widget-body">
 
-                <form action="<?php echo base_url("news/update/$item->id") ?>" method="post" enctype="multipart/form-data">
+                <form action="<?php echo base_url("references/update/$item->id") ?>" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <label>Başlık</label>
                         <input class="form-control" placeholder="Başlık" name="title" value="<?= $item->title; ?>">
@@ -24,58 +24,20 @@
                         </textarea>
                     </div>
 
-                    <div class="form-group">
-                        <label for="control-demo-6">Single Select</label>
-                        <div id="control-demo-6" style="width: 50%;">
-                            <?php if (isset($form_error)) : ?>
-                                <select class="form-control news_type_select" name="news_type">
-                                    <option value="image" <?php echo ($news_type == "image") ? "selected" : ""; ?>>Resim</option>
-                                    <option value="video" <?php echo ($news_type == "video") ? "selected" : ""; ?>>Video</option>
-                                </select>
-                            <?php else : ?>
-                                <select class="form-control news_type_select" name="news_type">
-                                    <option value="image" <?php echo ($item->news_type == "image") ? "selected" : ""; ?>>Resim</option>
-                                    <option value="video" <?php echo ($item->news_type == "video") ? "selected" : ""; ?>>Video</option>
-                                </select>
-                            <?php endif; ?>
-                        </div>
-                    </div><!-- .form-group -->
-
-                    <?php if (isset($form_error)) : ?>
-                        <div class="form-group image_upload_container" style="display: <?php echo ($news_type == "image") ? "block" : "none"; ?>">
-                            <label>Görsel Seçiniz</label>
-                            <input type="file" name="img_url" class="form-control" style="width: 50%;">
-                        </div>
-
-                        <div class="form-group video_url_container" style="display: <?php echo ($news_type == "video") ? "block" : "none"; ?>">
-                            <label>Video URL</label>
-                            <input class="form-control" placeholder="Video bağlantısını buraya yapıştırınız." name="video_url" style="width: 50%;">
-                            <?php if (isset($form_error)) : ?>
-                                <small class="pull-right input-form-error "><?php echo form_error("video_url"); ?></small>
-                            <?php endif; ?>
-                        </div>
-                    <?php else : ?>
-                        <div class="form-group image_upload_container" style="display: <?php echo ($item->news_type == "image") ? "block" : "none"; ?>">
-                            <div class="row">
-                                <div class="col-md-2">
-
-                                    <img height="100" class="img-responsive" src="<?php echo base_url("uploads/$viewFolder/$item->img_url") ?>" alt="">
-                                </div>
-                                <div class="col-md-8">
-                                    <label>Görsel Seçiniz</label>
-                                    <input type="file" name="img_url" class="form-control" style="width: 50%;">
-                                </div>
+                    <div class="form-group image_upload_container">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <img height="100" class="img-responsive" src="<?php echo base_url("uploads/$viewFolder/$item->img_url") ?>" alt="">
+                            </div>
+                            <div class="col-md-8">
+                                <label>Görsel Seçiniz</label>
+                                <input type="file" name="img_url" class="form-control" style="width: 50%;">
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group video_url_container" style="display: <?php echo ($item->news_type == "video") ? "block" : "none"; ?>">
-                            <label>Video URL</label>
-                            <input class="form-control" placeholder="Video bağlantısını buraya yapıştırınız." name="video_url" style="width: 50%;" value="<?= $item->video_url ?>">
-
-                        </div>
-                    <?php endif; ?>
                     <button type="submit" class="btn btn-primary btn-md btn-outline">Güncelle</button>
-                    <a href="<?php echo base_url("news/index"); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+                    <a href="<?php echo base_url("references/index"); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
                 </form>
             </div><!-- .widget-body -->
         </div>
