b7bccda37fd47d1d3d2523f50aa8e1de7a9351dc
nurcan1 <nrcnplt93@gmail.com>
2021-04-28 14:42:03 +0300

    Galeri controller ve model olusturulmasi, Geleri listeleme sayfasinin yapimi.

---
+++ panel/application/views/galleries_v/list/content.php
@@ -0,0 +1,53 @@
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Galeri Listesi
+            <a href="<?php echo base_url("galleries/new_form"); ?>" class="btn btn-outline btn-primary btn-xs pull-right"> <i class="fa fa-plus"></i> Yeni Ekle</a>
+        </h4>
+    </div><!-- END column -->
+    <div class="col-md-12">
+        <div class="widget p-lg">
+
+            <?php if (empty($items)) { ?>
+                <div class="alert alert-info text-center">
+                    <p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="<?php echo base_url("galleries/new_form"); ?>">tıklayınız.</a></p>
+                </div>
+            <?php } else { ?>
+                <table class="table table-hover table-striped table-bordered content-container">
+
+                    <thead>
+                        <th class="order"><i class="fa fa-reorder"></i></th>
+                        <th class="w50">#id</th>
+                        <th>Galeri Adı</th>
+                        <th>Galeri Türü</th>
+                        <th>Klasör Adı</th>
+                        <th>Url</th>
+                        <th>Durumu</th>
+                        <th>İşlem</th>
+                    </thead>
+                    <tbody class="sortable" data-url=" <?php echo base_url("galleries/rankSetter"); ?>">
+                        <?php foreach ($items as $item) { ?>
+                            <tr id="ord-<?= $item->id; ?>">
+                                <td class="order"><i class="fa fa-reorder"></i></td>
+                                <td clas="w50 text-center"># <?php echo $item->id; ?></td>
+                                <td><?php echo $item->gallery_name; ?></td>
+                                <td><?php echo $item->gallery_type; ?></td>
+                                <td><?php echo $item->folder_name; ?></td>
+                                <td><?php echo $item->url; ?></td>
+                                <td>
+                                    <input data-url="<?php echo base_url("galleries/isActiveSetter/$item->id"); ?>" class="isActive" type="checkbox" data-switchery data-color="#10c469" <?php echo ($item->isActive) ? "checked" :  " "; ?> />
+                                </td>
+                                <td>
+                                    <button data-url="<?php echo base_url("galleries/delete/$item->id"); ?>" class="btn btn-sm btn-danger btn-outline remove-btn">
+                                        <i class="fa fa-trash"></i> Sil
+                                    </button>
+                                    <a href="<?php echo base_url("galleries/update_form/$item->id"); ?>" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
+                                    <a href="<?php echo base_url("galleries/image_form/$item->id"); ?>" class="btn btn-sm btn-dark btn-outline"><i class="fa fa-image"></i> Resimler</a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            <?php } ?>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>
